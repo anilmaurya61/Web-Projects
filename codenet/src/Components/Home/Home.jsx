4460a44f44b05ba0b9b39b8f80a4af53e85de0fb
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-09-02 05:56:07 +0530

    updated

---
+++ codenet/src/Components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "Style.css";
+
+export default function home() {
+    return (
+        <div>
+           <header>
+
+           </header>
+           <div className="container">
+               <div className="left-container">
+
+               </div>
+               <div className="right-container">
+                   
+               </div>
+           </div>
+        </div>
+    )
+}
