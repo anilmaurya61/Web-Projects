4dc1e1f0ca9cd1fc861832b9cddb79da89e38af7
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-09-02 14:39:25 +0530

    updated

--- codenet/src/Components/LBsheetAPI.js
+++ codenet/src/Components/LBsheetAPI.js
@@ -1,27 +1,89 @@
 const LoveSheet = [
     {
-        ID : "1",
-        DSName : "Stack",
-        started : false,
-        attempted : "0",
-        flag : "0",
-        point : "0",
-        submission:"0",
+        ID: "1",
+        DSName: "Stack",
+        started: false,
+        attempted: "0",
+        flag: "0",
+        point: "0",
+        submission: "0",
         problems: [
             {
-            id : "1",
-            ds : "Stack",
-            probleName:"Reverse String",
-            done : false,
-            mark : false,
-            difficultylevel :"Easy",
-            tags :"stack",
-            url : "...........",
-            }
+                id: "1",
+                ds: "Stack",
+                probleName: "Reverse String",
+                done: false,
+                mark: false,
+                difficultylevel: "Easy",
+                tags: "stack",
+                url: "...........",
+            },
+            {
+                id: "1",
+                ds: "Stack",
+                probleName: "Reverse String",
+                done: false,
+                mark: false,
+                difficultylevel: "Easy",
+                tags: "stack",
+                url: "...........",
+            },
+            {
+                id: "1",
+                ds: "Stack",
+                probleName: "Reverse String",
+                done: false,
+                mark: false,
+                difficultylevel: "Easy",
+                tags: "stack",
+                url: "...........",
+            },
         ],
-        
+
 
     },
+    {
+        ID: "1",
+        DSName: "Stack",
+        started: false,
+        attempted: "0",
+        flag: "0",
+        point: "0",
+        submission: "0",
+        problems: [
+            {
+                id: "1",
+                ds: "Stack",
+                probleName: "Reverse String",
+                done: false,
+                mark: false,
+                difficultylevel: "Easy",
+                tags: "stack",
+                url: "...........",
+            },
+            {
+                id: "1",
+                ds: "Stack",
+                probleName: "Reverse String",
+                done: false,
+                mark: false,
+                difficultylevel: "Easy",
+                tags: "stack",
+                url: "...........",
+            },
+            {
+                id: "1",
+                ds: "Stack",
+                probleName: "Reverse String",
+                done: false,
+                mark: false,
+                difficultylevel: "Easy",
+                tags: "stack",
+                url: "...........",
+            },
+    
+        ],
+    }
 ];
 
 export default LoveSheet;
