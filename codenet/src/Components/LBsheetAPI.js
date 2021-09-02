09db13fe4828b5456db4788bc7c2fa8a7396c331
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-09-02 14:37:06 +0530

    updated

---
+++ codenet/src/Components/LBsheetAPI.js
@@ -0,0 +1,27 @@
+const LoveSheet = [
+    {
+        ID : "1",
+        DSName : "Stack",
+        started : false,
+        attempted : "0",
+        flag : "0",
+        point : "0",
+        submission:"0",
+        problems: [
+            {
+            id : "1",
+            ds : "Stack",
+            probleName:"Reverse String",
+            done : false,
+            mark : false,
+            difficultylevel :"Easy",
+            tags :"stack",
+            url : "...........",
+            }
+        ],
+        
+
+    },
+];
+
+export default LoveSheet;
