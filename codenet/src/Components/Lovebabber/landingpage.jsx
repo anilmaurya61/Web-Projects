5fd4f670a6950e3f15c24d901e0fabcd3b20993e
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-09-04 12:31:23 +0530

    update

--- codenet/src/Components/Lovebabber/landingpage.jsx
+++ codenet/src/Components/Lovebabber/landingpage.jsx
@@ -6,7 +6,14 @@ export default function landingpage() {
         <div className="page-1">
             <header>CodeNet🥰</header>
             <div className="container-1">
-                <div className="menu">Array</div>
+                <div className="menu">
+                    <ul className="list-items">
+                        <li className="list">Array</li>
+                        <li className="list">Stack</li>
+                        <li className="list">Graph</li>
+                        <li className="list">Queue</li>
+                    </ul>
+                </div>
                 <div className="container">Card</div>
             </div>
             <div className="footer"></div>
