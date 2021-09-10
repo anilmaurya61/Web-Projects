95b6fa698dc76b9427c13558c49cd1a4a55d68e3
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-09-10 21:03:55 +0530

    updated

--- codenet/src/Components/Lovebabber/landingpage.jsx
+++ codenet/src/Components/Lovebabber/landingpage.jsx
@@ -4,7 +4,24 @@ import './landingpage.css';
 export default function landingpage() {
     return (
         <div className="page-1">
-            <header>CodeNet🥰</header>
+            <header>
+                <div className="item-left">
+                    CodeNet
+                </div>
+                <div className="item-center">
+                    <ul>
+                        <li>LoveBabber Sheet</li>
+                        <li>Striver Sheet</li>
+                        <li>Faraz Sheet</li>
+                    </ul>
+                </div>
+                <div className="item-right">
+                    <ul>
+                        <li>SignUp</li>
+                        <li>Login</li>
+                    </ul>
+                </div>
+            </header>
             <div className="container-1">
                 <div className="menu">
                     <ul className="list-items">
@@ -14,7 +31,33 @@ export default function landingpage() {
                         <li className="list">Queue</li>
                     </ul>
                 </div>
-                <div className="container">Card</div>
+                <div className="container">
+                    <div className="card">
+                        
+                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, quae?</div>
+                        <div className="progressbar">
+                            <div class="circular">
+                                <div class="inner"></div>
+                                <div class="number">100%</div>
+                                <div class="circle">
+                                    <div class="bar left">
+                                        <div class="progress"></div>
+                                    </div>
+                                    <div class="bar right">
+                                        <div class="progress"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, quisquam.
+                    </div>
+                    <div className="card">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, quisquam.
+                    </div>
+
+                </div>
             </div>
             <div className="footer"></div>
         </div>
