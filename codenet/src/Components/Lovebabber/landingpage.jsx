91dcd3e0802ea7fea7d96b19757595eca8366d21
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-09-10 22:43:51 +0530

    updated

--- codenet/src/Components/Lovebabber/landingpage.jsx
+++ codenet/src/Components/Lovebabber/landingpage.jsx
@@ -3,63 +3,133 @@ import './landingpage.css';
 
 export default function landingpage() {
     return (
-        <div className="page-1">
+        <>
             <header>
-                <div className="item-left">
-                    CodeNet
+                <div className="logo-container">
+                    <img src="./img/logo.svg" alt="logo" />
+                    <h4 className="logo">CodeNet</h4>
                 </div>
-                <div className="item-center">
-                    <ul>
-                        <li>LoveBabber Sheet</li>
-                        <li>Striver Sheet</li>
-                        <li>Faraz Sheet</li>
+                <nav>
+                    <ul className="nav-links">
+                        <li><a className="nav-link" href="#">Love Babber Sheet</a></li>
+                        <li><a className="nav-link" href="#">Striver Sheet</a></li>
+                        <li><a className="nav-link" href="#">Faraz Sheet</a></li>
                     </ul>
-                </div>
-                <div className="item-right">
-                    <ul>
-                        <li>SignUp</li>
-                        <li>Login</li>
-                    </ul>
+                </nav>
+                <div className="cart">
+                    <img src="./img/cart.svg" alt="cart" />
                 </div>
             </header>
-            <div className="container-1">
-                <div className="menu">
-                    <ul className="list-items">
-                        <li className="list">Array</li>
-                        <li className="list">Stack</li>
-                        <li className="list">Graph</li>
-                        <li className="list">Queue</li>
-                    </ul>
+
+            <main>
+                <section className="presentation">
+                    <div className="introduction">
+                        <div className="intro-text">
+                            <h1>Welcome To CodeNet</h1>
+                            <p>
+                                ................
+                            </p>
+                        </div>
+                        <div className="cta">
+                            <button className="cta-select">Login</button>
+                            <button className="cta-add">SignUp</button>
+                        </div>
+                    </div>
+                    <div className="cover">
+                        <img src="./img/matebook.png" alt="matebook" />
+                    </div>
+                </section>
+
+                <div className="laptop-select">
+                    <img src="./img/arrow-left.svg" alt="" />
+                    <img src="./img/dot.svg" alt="" />
+                    <img src="./img/dot-full.svg" alt="" />
+                    <img src="./img/dot-full.svg" alt="" />
+                    <img src="./img/arrow-right.svg" alt="" />
                 </div>
-                <div className="container">
-                    <div className="card">
-                        
-                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, quae?</div>
-                        <div className="progressbar">
-                            <div class="circular">
-                                <div class="inner"></div>
-                                <div class="number">100%</div>
-                                <div class="circle">
-                                    <div class="bar left">
-                                        <div class="progress"></div>
+
+                <img className="big-circle" src="./img/big-eclipse.svg" alt="" />
+                <img className="mid-circle" src="./img/mid-eclipse.svg" alt="" />
+                <img className="small-circle" src="./img/small-eclipse.svg" alt="" />
+            </main>
+
+            {/* ............................................................. */}
+
+            <div class="teamWrapper">
+                <div class="container">
+                    <h1 align="center">Our Team</h1>
+                    <p align="center">Something about our team</p>
+                    <div class="teamGrid">
+                        <div class="colmun">
+                            <div class="teamcol">
+                                <div class="teamcolinner">
+                                    <div class="avatar"><img src="https://i.ibb.co/6nQP7Wm/pexels-vinicius-wiesehofer-1130626-1.jpgjpg" alt="Member" /></div>
+                                    <div class="member-name"> <h2 align="center">Ketty Leroux</h2> </div>
+                                    <div class="member-info"><p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p></div>
+                                    <div class="member-mail"> <p align="center"> <a href="mailto:kettyleroux@gmail.com">kettyleroux@gmail.com</a> </p> </div>
+                                    <div class="member-social">
+                                        <ul class="social-listing">
+                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        </ul>
                                     </div>
-                                    <div class="bar right">
-                                        <div class="progress"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="colmun">
+                            <div class="teamcol">
+                                <div class="teamcolinner">
+                                    <div class="avatar"><img src="https://i.ibb.co/6nQP7Wm/pexels-vinicius-wiesehofer-1130626-1.jpg" alt="Member" /></div>
+                                    <div class="member-name"> <h2 align="center">Ketty Leroux</h2> </div>
+                                    <div class="member-info"><p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p></div>
+                                    <div class="member-mail"> <p align="center"> <a href="mailto:kettyleroux@gmail.com">kettyleroux@gmail.com</a> </p> </div>
+                                    <div class="member-social">
+                                        <ul class="social-listing">
+                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="colmun">
+                            <div class="teamcol">
+                                <div class="teamcolinner">
+                                    <div class="avatar"><img src="https://i.ibb.co/6nQP7Wm/pexels-vinicius-wiesehofer-1130626-1.jpg" alt="Member" /></div>
+                                    <div class="member-name"> <h2 align="center">Ketty Leroux</h2> </div>
+                                    <div class="member-info"><p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p></div>
+                                    <div class="member-mail"> <p align="center"> <a href="mailto:kettyleroux@gmail.com">kettyleroux@gmail.com</a> </p> </div>
+                                    <div class="member-social">
+                                        <ul class="social-listing">
+                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="card">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, quisquam.
-                    </div>
-                    <div className="card">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, quisquam.
-                    </div>
-
                 </div>
             </div>
-            <div className="footer"></div>
-        </div>
+
+            {/* ..................footer................................ */}
+
+            <div class="footer-basic">
+                <footer>
+                    <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#">Home</a></li>
+                        <li class="list-inline-item"><a href="#">Services</a></li>
+                        <li class="list-inline-item"><a href="#">About</a></li>
+                        <li class="list-inline-item"><a href="#">Terms</a></li>
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    </ul>
+                    <p class="copyright">Company Name © 2018</p>
+                </footer>
+            </div>
+        </>
     )
 }
